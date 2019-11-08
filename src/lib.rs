6bb17d32fa681edb9f4925b4b6f78e45be145337
Linkun Chen <lkchen_github@outlook.com>
2019-11-08 02:17:35 -0800

    Finish NodeId parse without test
    test requires Eq trait.

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,5 @@
+#[macro_use]
+extern crate more_asserts;
 extern crate dot;
 
 mod ll_parser;
@@ -5,5 +7,5 @@ mod ll_parser;
 pub fn parse(graph_str: &str) {
     println!("File content:");
     println!("{}", graph_str);
-    ll_parser::parse_graph(graph_str);
+    // ll_parser::parse_graph(graph_str);
 }
--- src/ll_parser.rs
+++ src/ll_parser.rs
@@ -2,6 +2,16 @@ extern crate dot;
 
 use std::collections::HashMap;
 
+macro_rules! matches(
+    ($e:expr, $p:pat) => (
+        match $e {
+            $p => true,
+            _ => false
+        }
+    )
+);
+
+#[derive(Copy, Clone)]
 enum CompassPt {N, NE, E, SE, S, SW, W, NW, C, UND}
 
 pub struct Stmt<'a> {
@@ -14,14 +24,112 @@ pub struct Stmt<'a> {
 
 type StmtList<'a> = Vec<Stmt<'a>>;
 
+#[derive(Default)]
 pub struct Port<'a> {
     id: Option<dot::Id<'a>>,
-    compass_pt: Option<CompassPt>
+    compass_pt: Option<CompassPt>,
+}
+
+impl PartialEq for Port<'_> {
+    fn eq(&self, other: &Self) -> bool {
+        fn id_match(lhs: &Port, rhs: &Port) -> bool {
+            match ((*lhs).id, (*rhs).id) {
+                (None, None) => true,
+                (Some(a), Some(b)) => a.name() == b.name(),
+                _ => false,
+            }
+        }
+
+        match (self.compass_pt, other.compass_pt) {
+            (None, None) => id_match(self, other),
+            (Some(a), Some(b)) => !matches!(a, b) && id_match(self, other),
+            _ => false,
+        }
+    }
+}
+
+impl<'a> Port<'a> {
+    fn parse_from(tokens: Vec<&str>) -> Result<Port<'_>, ()> {
+        if tokens.len() == 2 {
+            if tokens[0] != ":" {
+                return Err(());
+            } else {
+                match Port::parse_compass_pt_from(tokens[1]) {
+                    Ok(comp) => Ok(Port{compass_pt: Some(comp), ..Default::default()}),
+                    Err(_) => {
+                        match dot::Id::new(tokens[1]) {
+                            Ok(id) => Ok(Port{id: Some(id), ..Default::default()}),
+                            Err(_) => Err(()),
+                        }
+                    }
+                }
+            }
+        } else if tokens.len() == 4 {
+            if tokens[0] != ":" || tokens[2] != ":" {
+                return Err(())
+            } else {
+                match Port::parse_compass_pt_from(tokens[3]) {
+                    Err(_) => Err(()),
+                    Ok(comp) => {
+                        match dot::Id::new(tokens[1]) {
+                            Ok(id) => Ok(Port{id: Some(id), compass_pt: Some(comp)}),
+                            Err(_) => Err(())
+                        }
+                    }
+                }
+            }
+        } else {
+            Err(())
+        }
+    }
+
+    fn parse_compass_pt_from(token: &str) -> Result<CompassPt, ()> {
+        match token {
+            "n" => Ok(CompassPt::N),
+            "ne" => Ok(CompassPt::NE),
+            "e" => Ok(CompassPt::E) ,
+            "se" => Ok(CompassPt::SE),
+            "s" => Ok(CompassPt::S),
+            "sw" => Ok(CompassPt::SW),
+            "w" => Ok(CompassPt::W),
+            "nw" => Ok(CompassPt::NW),
+            "c" => Ok(CompassPt::C),
+            "_" => Ok(CompassPt::UND),
+            _ => Err(()),
+        }
+    }
 }
 
 pub struct NodeId<'a> {
     id: dot::Id<'a>,
     port: Option<Port<'a>>
+}
+
+impl<'a> NodeId<'a> {
+    fn parse_from(tokens: Vec<&str>) -> Result<NodeId<'_>, ()> {
+        if tokens.len() == 0 {
+            return Err(());
+        }
+        match dot::Id::new(tokens[0]) {
+            Err(_) => Err(()),
+            Ok(id) => {
+                match NodeId::parse_option_port_from(tokens[1..].to_vec()) {
+                    Err(_) => Err(()),
+                    Ok(port) => Ok(NodeId{id: id, port: port}),
+                }
+            }
+        }
+    }
+
+    fn parse_option_port_from(tokens: Vec<&str>) -> Result<Option<Port<'_>>, ()> {
+        if tokens.len() == 0 {
+            return Ok(None)
+        }
+        match Port::parse_from(tokens) {
+            Ok(port) => Ok(Some(port)),
+            Err(_) => Err(()),
+        }
+    }
 }
 
 type AList<'a> = HashMap<dot::Id<'a>, dot::Id<'a>>;
@@ -65,10 +173,19 @@ pub struct Graph<'a> {
     stmt_list: StmtList<'a>
 }
 
+// impl NodeStmt<'_> {
+//     fn parse_from(tokens: Vec<&str>) -> Result<NodeStmt<'_>, &'static str> {
+
+//     }
+// }
+
 #[cfg(test)]
 mod tests {
+    use super::{NodeId};
+
     #[test]
-    fn s() {
-        assert!(true);
+    fn parse_nodeid() {
+        let n1 = NodeId::parse_from(["id1"].to_vec()).unwrap();
+        assert_eq!(n1.id.name(), "id1");
     }
 }
